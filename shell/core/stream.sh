48c4c6df914ccc0042e62ff8402f4085bf14bfdf
jk977 <jdk64@comcast.net>
2017-08-15 15:55:58 -0700

    Added more options for stream

--- shell/core/stream.sh
+++ shell/core/stream.sh
@@ -4,7 +4,14 @@
 
 dir="$( dirname $0 )"
 source "$dir/../config.sh"
-audio_opts="-f alsa -ar 22050 -i default"
-video_opts="-f x11grab -r 60 -s 256x240 -i :0.0 -c:v libx264 -preset medium -pix_fmt yuv420p -s 256x240 -threads 4 -b 30k -f flv"
+
+audio_opts="-f alsa -ar 44100 -i default"
+video_opts="-f x11grab -r 30 -s 256x240 -i :0.0+0,36 -c:v libx264 -preset medium -pix_fmt yuv420p -s 256x240 -threads 4 -b 20k -f flv"
+
+#uncomment for better video quality
+#video_opts="-f x11grab -r 30 -s 256x240 -i :0.0+0,36 -c:v libx264 -preset slow -pix_fmt yuv420p -s 256x240 -threads 4 -b 30k -f flv"
+
+#uncomment to save stream to file
+#stream="/home/pi/Desktop/stream.flv"
 
 avconv $audio_opts $video_opts "$stream" 2>&1 | tee "$logdir/stream.log"
