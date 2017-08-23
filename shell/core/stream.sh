4398bcbba737697fa551e57c021a8d988e34a4f0
jk977 <jdk64@comcast.net>
2017-08-22 21:30:57 -0700

    Replacing avconv with ffmpeg and making stream infinitely loop the input audio instead of restarting the stream every time audio ends

--- shell/core/stream.sh
+++ shell/core/stream.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 # Starts streaming to Twitch.
-# TODO fix audio to fully work instead of alternating on and off every ~0.5 seconds 
 
 dir="$( dirname $0 )"
 source "$dir/../config.sh"
 
-audio_opts="-f mp3 -i $audiodir/all.mp3"
-song_len=3196
-video_opts="-f x11grab -r 30 -s 256x240 -i :0.0+0,36 -c:v libx264 -preset slow -pix_fmt yuv420p -s 256x240 -threads 4 -b 30k -f flv"
-
-#uncomment for better video quality
-#video_opts="-f x11grab -r 30 -s 256x240 -i :0.0+0,36 -c:v libx264 -preset slow -pix_fmt yuv420p -s 256x240 -threads 4 -b 30k -f flv"
-
-#uncomment to save stream to file
 #stream="/home/pi/Desktop/stream.flv"
 
-while :
-do
-    avconv $audio_opts $video_opts -aq 5 -t $song_len "$stream" 2>&1 | tee "$logdir/stream.log"
-done
+ffmpeg \
+    -stream_loop -1 -f mp3 -i "$audiodir/all.mp3" \
+    -thread_queue_size 64 -threads 4 -f x11grab -r 30 -s 256x240 -i :0.0+0,36 \
+    -c:v libx264 -preset fast -pix_fmt yuv420p \
+    -c:a copy -ar 44100 -ac 2 \
+    -f flv "$stream" 2>&1 | tee "$logdir/stream.log"
