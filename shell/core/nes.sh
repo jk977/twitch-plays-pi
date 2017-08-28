1676915031845be0673b3d2ebbf4553e23275aec
jk977 <jdk64@comcast.net>
2017-08-27 21:19:18 -0700

    Adding game audio to output stream. Note that on the RPi it makes the video choppy - may need to keep audio overlay as primary option unless on a more powerful device

--- shell/core/nes.sh
+++ shell/core/nes.sh
@@ -9,5 +9,8 @@ fceux "$romdir" \
     --loadlua "$emudir/main.lua" \
     --xscale 1 \
     --yscale 1 \
-    --sound 0 \
+    --sound 1 \
+    --soundrate 22050 \
+    --soundq 0 \
+    --soundbufsize 200 \
     2>&1 | tee -a "$logdir/nes.log"
--- shell/core/stream.sh
+++ shell/core/stream.sh
@@ -4,17 +4,11 @@
 dir="$( dirname $0 )"
 source "$dir/../config.sh"
 
-audiofile="$audiodir/all.mp3"
-
 # uncomment to save stream to a file
 #stream="/home/pi/Desktop/stream.flv"
 
-while :
-do
-    ffmpeg \
-        -i "$audiofile" \
-        -threads 4 -f x11grab -r 30 -s 256x240 -i :0.0+0,36 \
-        -c:a copy -ar 44100 -ac 2 \
-        -c:v libx264 -preset slow -pix_fmt yuv420p \
-        -shortest -f flv "$stream" 2>&1 | tee -a "$logdir/stream.log"
-done
+ffmpeg \
+    -thread_queue_size 64 -threads 4 -f x11grab -r 30 -s 256x240 -i :0.0+0,36 \
+    -thread_queue_size 64 -f alsa -ar 22050 -i default \
+    -c:v libx264 -preset medium -pix_fmt yuv420p -b:v 20k \
+    -f flv "$stream" 2>&1 | tee -a "$logdir/stream.log"
