95248d6b0319d3fbb3e50393fd40742bc525b78e
jk977 <jdk64@comcast.net>
2017-09-09 01:57:18 -0700

    Cleaning config.py

--- bot/config.py
+++ bot/config.py
@@ -3,25 +3,17 @@ from emulator import Emulator
 from threads.stoppablepool import StoppablePool
 from votes.votemanager import VoteManager
 
-
-# IRC configuration
-# =================
-
 HOST = 'irc.twitch.tv'
 PORT = 6667
 NICK = 'shira_bot'
-RATE = 2
 
 with open('info/host.cfg', 'r') as file:
-    CHAN = file.read().strip() # bot owner; strip in case of unintended whitespace
+    CHAN = file.read().strip() # bot owner; strip in case of whitespace
 
 with open('info/oauth.cfg', 'r') as file:
     PASS = 'oauth:' + file.read().strip()
 
-
-# Bot configuration
-# =================
-
+# tracks all threads used by bot
 threads = StoppablePool()
 
 # used in vote tracking
