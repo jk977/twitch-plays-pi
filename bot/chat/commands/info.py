79e578ae230afe02e5aaf7dd6b1bb0a20a3e5762
jk977 <jdk64@comcast.net>
2017-11-20 18:58:30 -0800

    Fixing bot commands to work with the recent changes

--- bot/chat/commands/info.py
+++ bot/chat/commands/info.py
@@ -2,9 +2,11 @@ import config
 import os
 
 def show_help(chat, message):
-    path = os.path.join(config.info_dir, 'help.cfg')
+    path = os.path.join(config.bot_dir, 'help.dat')
+
     with open(path, 'r') as file:
         help_link = file.read()
+
     chat.send_message(help_link)
 
 def song(chat, message):
@@ -12,4 +14,9 @@ def song(chat, message):
                       'https://www.youtube.com/playlist?list=PL2jLKwo6ZTmQ0vKgGwbcp5m2sp7vy9GXY')
 
 def game_map(chat, message):
-    chat.send_message('')
+    path = os.path.join(config.bot_dir, 'map.dat')
+
+    with open(path, 'r') as file:
+        map_link = file.read()
+
+    chat.send_message(map_link)
